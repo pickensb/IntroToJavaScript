aa9e82f5f8fff4b1476fa0f67a2b8bea5e928189
pickensb <70235552+pickensb@users.noreply.github.com>
2021-05-13 14:14:31 -0400

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-var name = prompt("What is your name?");
-var color = prompt("What is your favorite color?");
-document.body.querySelector(".message").innerHTML="My name is "+name+". My favorite color is "+color;
