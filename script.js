0af95b2c91ae2a04da232ad555fa02eacb5d2911
pickensb <70235552+pickensb@users.noreply.github.com>
2021-05-13 14:15:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var name = prompt("What is your name?");
+var color = prompt("What is your favorite color?");
+document.body.querySelector(".message").innerHTML="My name is "+name+" and my favorite color is "+color;
